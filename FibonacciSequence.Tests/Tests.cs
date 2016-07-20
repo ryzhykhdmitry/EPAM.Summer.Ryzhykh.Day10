d92480eafb785e547979f7a3a630d3c49ec3b5fd
Dmitry Ryzhykh <ryzhykh.dmitry@gmail.com>
2016-07-20 22:38:45 +0300

    Task 1 added

---
+++ FibonacciSequence.Tests/Tests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FibonacciSequence;
+
+namespace FibonacciSequence.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        #region TestData
+        public class DataClass
+        {
+            public static IEnumerable<TestCaseData> GetFibonacciSecuenceData
+            {
+                get
+                {
+                    yield return new TestCaseData(5).Returns(new long[] { 0, 1, 1, 2, 3 });
+                    yield return new TestCaseData(15).Returns(new long[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 });
+                }
+            }
+
+            public static IEnumerable<TestCaseData> GetFibonacciByIndexData
+            {
+                get
+                {
+                    yield return new TestCaseData(5).Returns(3);
+                    yield return new TestCaseData(15).Returns(377);
+                }
+            }
+        }
+        #endregion
+
+        #region FibonacciTests
+        [Test, TestCaseSource(typeof(DataClass), "GetFibonacciSecuenceData")]
+        public IEnumerable<long> GetFibonacciSequence_number_fibonacciSequence(int size)
+        {
+            return Fibonacci.GetFibonacciSequence(size);
+        }
+
+        [Test, TestCaseSource(typeof(DataClass), "GetFibonacciByIndexData")]
+        public long GetFibonacciByIndex_index_fibonacciNumber(int index)
+        {
+            return Fibonacci.GetFibonacciByIndex(index);
+        } 
+        #endregion
+    }
+}
---
+++ FibonacciSequence/FibonacciSequence.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSequence
+{
+    public class Fibonacci
+    {
+        
+        /// <summary>
+        /// Return fibonacci sequence.
+        /// </summary>
+        /// <param name="size">Number of elements</param>
+        /// <returns>Sequence of fibonachi elements</returns>
+        public static IEnumerable<long> GetFibonacciSequence(int size)
+        {
+            if (size <= 0)
+                throw new ArgumentException();
+
+            long previous = 0, current = 1;
+            for (int i = 0; i < size; i++)
+            {
+                yield return previous;
+                long next = previous + current;
+                previous = current;
+                current = next;
+            }
+        }
+
+        /// <summary>
+        /// Return value of fibonachi series on defenet place
+        /// </summary>
+        /// <param name="index">Index in fibonachi series</param>
+        /// <returns></returns>
+        public static long GetFibonacciByIndex(int index)
+        {
+            if (index <= 0)
+                throw new ArgumentException();
+
+            IEnumerable<long> series = GetFibonacciSequence(index);
+            return series.Last();
+        }
+        
+    }
+}
